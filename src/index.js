e6ae5fe9d376e113b30d27cfe5079587aa764c41
amekatze <setharie@gmail.com>
2022-12-11 13:50:55 +0900

    Update todo status in local storage on edit

--- src/index.js
+++ src/index.js
@@ -1,100 +1,116 @@
-import { format, compareAsc } from 'date-fns'
-import './style.css';
+import { format, compareAsc } from "date-fns";
+import "./style.css";
 
 // Dom
-const submitTodoDom = document.getElementById('submit');
-const todoListDom = document.getElementById('todolist');
-const taskInputDom = document.getElementById('taskinput');
-const dateInputDom = document.getElementById('dateinput');
-
+const submitTodoDom = document.getElementById("submit");
+const todoListDom = document.getElementById("todolist");
+const taskInputDom = document.getElementById("taskinput");
+const dateInputDom = document.getElementById("dateinput");
 
 //Event Listeners
-submitTodoDom.addEventListener('click', addTodo);
-todoListDom.addEventListener('click', actionTodo);
+submitTodoDom.addEventListener("click", addTodo);
+todoListDom.addEventListener("click", actionTodo);
 
+// Load JSON file
+let todos =
+  localStorage.length == 0 ? [] : JSON.parse(localStorage.getItem("todos"));
 
-let todos = localStorage.length == 0 ? [] : JSON.parse(localStorage.getItem("todos"));
+// Todo Factory
+const todoTask = (task, date, pending) => {
+  todos.push({
+    task,
+    date,
+    pending,
+  });
+  localStorage.setItem("todos", JSON.stringify(todos));
+  return { task, date, pending };
+};
 
-const todoTask = (task, date, complete) => {
-    todos.push({
-        task,
-        date,
-        complete
-    })
-    localStorage.setItem("todos", JSON.stringify(todos));
-    return {task, date, complete}
-}
-
-todos.forEach(todo => createTaskDom(todo.task, todo.date))
+todos.forEach((todo) => createTaskDom(todo.task, todo.date));
 
 //Functions
 // Create Todo Item
-function addTodo(event){
-    event.preventDefault(); //prevent refresh
+function addTodo(event) {
+  event.preventDefault(); //prevent refresh
 
-    todoTask(taskInputDom.value, dateInputDom.value, true);
-    createTaskDom(taskInputDom.value, dateInputDom.value);
-    taskInputDom.value = ''; 
+  todoTask(taskInputDom.value, dateInputDom.value, true);
+  createTaskDom(taskInputDom.value, dateInputDom.value);
+  taskInputDom.value = "";
 }
 
 // Create Item in Dom
-function createTaskDom(task, date){
-    const todoItem = document.createElement('li');
-    const check = document.createElement('button');
-    const taskItem = document.createElement('input');
-    const taskDate = document.createElement('input');
-    const del = document.createElement('button');
-    
-    todoItem.classList.add('pending');
-    check.classList.add('status');
-    taskItem.classList.add('task');
-    del.classList.add('delete');
+function createTaskDom(task, date) {
+  const todoItem = document.createElement("li");
+  const check = document.createElement("button");
+  const taskItem = document.createElement("input");
+  const taskDate = document.createElement("input");
+  const del = document.createElement("button");
 
-    taskItem.type = "text";
-    taskItem.readOnly = true;
-    taskDate.type = "date";
-    check.innerHTML = '<i class="fa-regular fa-circle fa-xl"></i>';
-    del.innerHTML = '<i class="fa-regular fa-trash-can fa-xl"></i>';
+  setTodoStatusInDOM(task, todoItem, check);
+  check.classList.add("status");
+  taskItem.classList.add("task");
+  del.classList.add("delete");
 
-    taskItem.value = task;
-    taskDate.value = date;
+  taskItem.type = "text";
+  taskItem.readOnly = true;
+  taskDate.type = "date";
+  del.innerHTML = '<i class="fa-regular fa-trash-can fa-xl"></i>';
 
-    todoItem.append(check,taskItem,taskDate,del);
-    todoListDom.appendChild(todoItem); 
+  taskItem.value = task;
+  taskDate.value = date;
+
+  todoItem.append(check, taskItem, taskDate, del);
+  todoListDom.appendChild(todoItem);
 }
 
 // TodoList Actions
-function actionTodo(e){
-    const item = e.target;
-    const action = item.classList[0];
-    const listItem = item.parentElement;
-    const todoTask = listItem.children[1].value;
-    // Delete Todo
-    if( action == 'delete'){
-        removeTodoFromStorage(todoTask);
-        item.parentElement.remove();
-    }
+function actionTodo(e) {
+  const item = e.target;
+  const action = item.classList[0];
+  const listItem = item.parentElement;
+  const todoTask = listItem.children[1].value;
+  // Delete Todo
+  if (action == "delete") {
+    removeTodoFromStorage(todoTask);
+    item.parentElement.remove();
+  }
 
-    if( action == 'status'){
-        if (listItem.classList[0] == 'pending'){
-            listItem.classList.remove('pending');
-            listItem.classList.add('complete');
-            item.innerHTML = '<i class="fa-regular fa-circle-check fa-xl"></i>';
-        } else {
-            listItem.classList.remove('complete');
-            listItem.classList.add('pending');
-            item.innerHTML = '<i class="fa-regular fa-circle fa-xl"></i>';
-        }
-    }
-
+  if (action == "status") {
+    editTodoStatusInStorage(todoTask, "pending");
+    setTodoStatusInDOM(todoTask, listItem, item);
+  }
 }
 
-function removeTodoFromStorage(todoTask){
-    const todoIndex = todos.findIndex(todo => {
-        return todo.task == todoTask;
-    })
-    todos.splice(todoIndex, 1);
-    localStorage.setItem("todos", JSON.stringify(todos));
+function removeTodoFromStorage(todoTask) {
+  let todoIndex = getIndexFromTodoJSON(todoTask);
+  todos.splice(todoIndex, 1);
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
 
+function editTodoStatusInStorage(todoTask, key) {
+  let todoIndex = getIndexFromTodoJSON(todoTask);
+  if (todos[todoIndex][key]) {
+    todos[todoIndex][key] = false;
+  } else todos[todoIndex][key] = true;
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
 
+function setTodoStatusInDOM(todoTask, todoStatusDOM, checkIcon) {
+  let todoIndex = getIndexFromTodoJSON(todoTask);
+  if (!todos[todoIndex].pending) {
+    todoStatusDOM.classList.add("complete");
+    todoStatusDOM.classList.remove("pending");
+    checkIcon.innerHTML = '<i class="fa-regular fa-circle-check fa-xl"></i>';
+  } else {
+    todoStatusDOM.classList.add("pending");
+    todoStatusDOM.classList.remove("complete");
+    checkIcon.innerHTML = '<i class="fa-regular fa-circle fa-xl"></i>';
+  }
+  console.log(todoStatusDOM);
+}
+
+function getIndexFromTodoJSON(todoTask) {
+  return todos.findIndex((todo) => {
+    return todo.task == todoTask;
+  });
+}
